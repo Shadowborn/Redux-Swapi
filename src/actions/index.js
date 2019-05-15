36e10667b9cbd471639e478dad651a28053da17e
Shadowborn <james.starks023@gmail.com>
2019-05-15 16:25:04 -0600

    List showing

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,24 @@
 // we'll need axios
-
+import axios from 'axios';
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
+export const FETCH_ = 'FETCH_';
+export const SUCCESS_ = 'SUCCESS_';
+export const FAILURE_ = 'FAILURE_';
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+export const fetchPosts = () => dispatch => {
+        dispatch({ type:FETCH_ })
+        axios
+        .get('https://swapi.co/api/people')
+        .then(res => {
+            dispatch({ type:SUCCESS_, payload:res.data })
+                // console.log("axios data", res.data);
+            })
+        .catch(err => {
+            dispatch({type:FAILURE_})
+            console.log(err);
+        })
+}
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,12 @@
-import /* we need our action types here*/ "../actions";
+import{ /* we need our action types here*/ 
+  FETCH_,
+  SUCCESS_,
+  FAILURE_,
+}from "../actions/index";
 const initialState = {
-  characters: []
+  characters: [],
+  fetching: false,
+  error: null,
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +14,18 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCH_: return {
+      ...state,
+      fetching : true,
+      error : '',
+    }
+    case SUCCESS_: return {
+      ...state,
+      fetching: false,
+      error: '',
+      characters: action.payload.results
+    }
+
     default:
       return state;
   }
